ebc43364522f32e1d4b5921b8d9cbf9e9d6e9ba7
pranu2502 <spranavreddy25@gmail.com>
2023-03-16 00:01:34 +0530

    Added Log4j

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
 import java.util.Scanner;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
 
+            BasicConfigurator.configure();
 
             System.out.println("Welcome to the Calculator App");
             System.out.println("Please enter your selected choice");
@@ -17,6 +23,7 @@ public class Main {
             Scanner sc = new Scanner(System.in);
 
             Calculator calculator = new Calculator();
+            logger.info("Calculator Initialised");
 
             int op = sc.nextInt();
 
@@ -29,12 +36,14 @@ public class Main {
                     case 1:
                         System.out.println("Enter a number");
                         num1 = sc.nextDouble();
+                        logger.info("Calculating the Square root of " + String.valueOf(num1));
                         System.out.println("Square Root of " + String.valueOf(num1) + " is " + calculator.squareRoot(num1));
                         break;
 
                     case 2:
                         System.out.println("Enter a number");
                         num1 = sc.nextDouble();
+                        logger.info("Calculating the factorial of " + String.valueOf(num1));
                         System.out.println("Factorial of " + String.valueOf(num1) + " is " + calculator.factorial(num1));
                         break;
 
@@ -42,6 +51,7 @@ public class Main {
                     case 3:
                         System.out.println("Enter a number");
                         num1 = sc.nextDouble();
+                        logger.info("Calculating the Natural Logarithm of " + String.valueOf(num1));
                         System.out.println("Natural Logarithm of " + String.valueOf(num1) + " is " + calculator.logarithm(num1));
                         break;
 
@@ -50,17 +60,19 @@ public class Main {
                         num1 = sc.nextDouble();
                         System.out.println("Enter a number for the power");
                         num2 = sc.nextDouble();
+                        logger.info("Calculating the value of " + String.valueOf(num1) + " raised to " + String.valueOf(num2));
                         System.out.println("Value of " + String.valueOf(num1) + " to the power of " + String.valueOf(num2) + " is " + calculator.power(num1, num2));
                         break;
 
                     default:
+                        logger.info("Invalid option entered");
                         System.out.println("Invalid Input");;
                         break;
                 }
 
                 op = sc.nextInt();
             }
-
+        logger.info("Exiting Calculator");
         System.out.println("Byeee");
 
         }
