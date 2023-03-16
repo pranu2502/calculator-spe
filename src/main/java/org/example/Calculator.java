3d43e79298a2a0e6a2421d3b3b72615ce4ed17fc
pranu2502 <spranavreddy25@gmail.com>
2023-03-16 12:43:22 +0530

    Added Tests for All functions

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,19 +9,19 @@ public class Calculator {
         return Math.sqrt(x);
     }
 
-    double factorial(double x)
+    public double factorial(double x)
     {
         if(x == 0)
             return 1;
         return x*factorial(x-1);
     }
 
-    double logarithm(double x)
+    public double logarithm(double x)
     {
         return Math.log(x);
     }
 
-    double power(double x, double y)
+    public double power(double x, double y)
     {
         return Math.pow(x, y);
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -18,4 +18,31 @@ public class CalculatorTest {
         assertEquals(1, calculator.squareRoot(1), DELTA);
     }
 
+    @Test
+    public void testFactorial()
+    {
+        assertEquals(6, calculator.factorial(3), DELTA);
+        assertEquals(40320, calculator.factorial(8), DELTA);
+        assertEquals(120, calculator.factorial(5), DELTA);
+        assertEquals(5040, calculator.factorial(7), DELTA);
+    }
+
+    @Test
+    public void testLogarithm()
+    {
+        assertEquals(6.894670039433482, calculator.logarithm(987), DELTA);
+        assertEquals(3.8066624897703196, calculator.logarithm(45), DELTA);
+        assertEquals(0.6931471805599453, calculator.logarithm(2), DELTA);
+        assertEquals(1.9459101490553132, calculator.logarithm(7), DELTA);
+    }
+
+    @Test
+    public void testPower()
+    {
+        assertEquals(8, calculator.power(2, 3), DELTA);
+        assertEquals(262144, calculator.power(4, 9), DELTA);
+        assertEquals(100, calculator.power(10, 2), DELTA);
+        assertEquals(4.747561509943E12, calculator.power(7, 15), DELTA);
+    }
+
 }
